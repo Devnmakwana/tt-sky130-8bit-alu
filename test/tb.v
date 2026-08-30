627032a03c92ea29583ed7daa19cadb945cea097
Devnmakwana <230130111046@gecg28.ac.in>
2026-08-30 13:45:01 +0530

    Ensure correct instance name and mapping in test bench

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_dev_makwana_alu8 (
+  tt_um_dev_makwana_alu8 user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
